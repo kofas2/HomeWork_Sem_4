c4fc589187108f389d6f808b3fec5be8d65e7919
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-20 22:12:02 +0300

    add Task002 Home work 4

---
+++ Task002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int MessageToUser(string message)
+{
+    System.Console.Write(message);
+    string userInput = System.Console.ReadLine();
+    int result = int.Parse(userInput);
+    return result;
+}
+
+int number = MessageToUser("Ввведите целое число: ");
+Console.WriteLine($"Сумма цифр в данном числе равна: {SumNumOfNum(number)}");
+
+
+int SumNumOfNum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number%10;
+        number = number/10;
+    }
+    return sum;
+}
